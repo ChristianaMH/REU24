ab86ee381717a142049b2d97bdbb6b2535ce5c07
ChristianaMH <106120377+ChristianaMH@users.noreply.github.com>
2024-06-30 12:38:22 -0400

    Update example_usage.py

--- example_usage.py
+++ example_usage.py
@@ -1,43 +1,31 @@
 import rclpy
-from rclpy.node import Node
-from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+import pkg_ros2_library_py.ros2_library as ros2lib #import library
+import time
 
-class RobotState: #define a class for robot state
-    def __init__(self):
-        self.linear_speed = 0.0 #initial state for linear speed
+def main(): #initialize using library
+    node = ros2lib.initialize_node('example_node')
 
-robot_state = RobotState() #instance for robot state
+    try:
+        # Create a velocity publisher
+        vel_publisher = ros2lib.create_velocity_publisher(node)
+        
+        # Set initial wheel speed (0.5 m/s)
+        ros2lib.set_wheel_speed(vel_publisher, 0.5) 
+        
+        # Allow some time to observe the speed change
+        for _  in range(20):
+            rclpy.spin_once(node, timeout_sec=0.1) #0.1 timeout to process incoming message/callback
+            current_speed = ros2lib.get_current_linear_speed() #gets current speed & prints it
+            print(f'Current linear speed: {current_speed}')
+            time.sleep(0.1) #0.1 delay
 
-def odom_callback(msg): #callback function for odometry message
-    # Extract the linear speed from the odometry message
-    linear_speed = msg.twist.twist.linear.x
-    robot_state.linear_speed = linear_speed #update linear speed
+        ros2lib.set_wheel_speed(vel_publisher, 0.0)  # Stop the robot (0.0 m/s)
+        
+    except KeyboardInterrupt: #stop program with key interrupt
+        pass
+    finally:
+        ros2lib.shutdown_node(node) #ensures proper shutdown
 
-def create_odom_subscriber(node): #subscription to /odom topic with queue size 10
-    # called when a new message is recieved 
-    node.create_subscription(Odometry, '/odom', odom_callback, 10)
+if __name__ == '__main__':
+    main() #executes main function
 
-def get_current_linear_speed(): #returns current speed of robot
-    return robot_state.linear_speed
-
-def create_velocity_publisher(node): #publisher to /cmd_vel topic with queue size 10
-    # allows the node to publish Twist message to control of robot's velocity 
-    return node.create_publisher(Twist, '/cmd_vel', 10)
-
-def set_wheel_speed(publisher, linear_speed, angular_speed=0.0): 
-# creates a Twist message to set robot's linear & angular speed 
-    msg = Twist()
-    msg.linear.x = linear_speed
-    msg.angular.z = angular_speed
-    publisher.publish(msg) #publish using publisher
-
-def initialize_node(node_name):
-    rclpy.init() # initialize ROS2 system
-    node = Node(node_name) #create a node 
-    create_odom_subscriber(node) #set up odometry subscriber 
-    return node
-
-def shutdown_node(node): #destroys node & shuts down system
-    node.destroy_node()
-    rclpy.shutdown()
